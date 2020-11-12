94d1685830b556fcdcc7c1d0ee2a4514f92341af
anomist3 <46968188+anomist3@users.noreply.github.com>
2020-11-12 23:49:18 +0900

    Add files via upload

--- js/dday.js
+++ js/dday.js
@@ -15,6 +15,12 @@ var toEsGraduation = esGraduation.getTime();
 var esGraduationTime = toEsGraduation - toNow;
 var esGraduationDday = Math.floor(esGraduationTime/(1000 * 60 * 60 * 24));
 
+var testDate = new Date("2022-1-1");
+var toTestDate = testDate.getTime();
+var testDateTime = toTestDate - toNow;
+var testDate = Math.round(testDateTime / (1000 * 60 * 60 * 24)); 
+
 document.querySelector("#school-festival").innerText = schoolFestivalDday + "일";
 document.querySelector("#winter-vacation").innerText = passedDay + "일";
 document.querySelector("#es-graduation").innerText = esGraduationDday + "일";
+document.querySelector("#test").innerText = testDate + "일";
