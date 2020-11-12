ebd89b53eeeeb1c681e5db91f497ca715a88464a
anomist3 <46968188+anomist3@users.noreply.github.com>
2020-11-12 20:56:49 +0900

    Update dday.js
    Deleted the msApplicationDay

--- js/dday.js
+++ js/dday.js
@@ -5,11 +5,6 @@ var toWinterVacation = winterVacation.getTime();
 var passedTime = toWinterVacation - toNow;
 var passedDay = Math.round(passedTime/(1000 * 60 * 60 * 24));
 
-var msApplication = new Date("2020-11-11");
-var toMsApplication = msApplication.getTime();
-var msApplicationTime = toMsApplication - toNow;
-var msApplicationDday = Math.round(msApplicationTime/(1000 * 60 * 60 * 24));
-
 var schoolFestival = new Date("2020-11-27");
 var toSchoolFestival = schoolFestival.getTime();
 var schoolFestivalTime = toSchoolFestival - toNow;
@@ -21,7 +16,6 @@ var esGraduationTime = toEsGraduation - toNow;
 var esGraduationDday = Math.round(esGraduationTime/(1000 * 60 * 60 * 24));
 
 document.querySelector("#accent").innerText = passedDay + "일";
-document.querySelector("#ms-application").innerText = msApplicationDday + "일";
 document.querySelector("#school-festival").innerText = schoolFestivalDday + "일";
 document.querySelector("#winter-vacation").innerText = passedDay + "일";
 document.querySelector("#es-graduation").innerText = esGraduationDday + "일";
